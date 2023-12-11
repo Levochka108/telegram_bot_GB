9e72f328b354dbcba3d5d91114b8441132ff09d6
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2023-12-11 16:31:36 +0300

    save code 11.12.2023

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import telebot
 
 
 bot = telebot.TeleBot((my_token()), parse_mode='html')
+user_states = {}
 
 
 @bot.message_handler(commands=['start'])
@@ -21,6 +22,8 @@ def start(message):
 
 @bot.message_handler(content_types="text")
 def message_replay(message):
+    user_state = user_states.get(message.chat.id, None)
+
     if message.text == "Go":
         text_mes: str = f"<b>DUNGEON OF SHADOWS</b> подарит тебе мир, полный тайн и волшебства. " \
             f"Пройдя до конца путь сражений и разгадав все загадки," \
@@ -88,37 +91,61 @@ def message_replay(message):
     elif message.text == "Уйти в свой замок":
         text_warrior = "Кажется, сегодня боги отвернулись от тебя. Выйдя из транса, старец с помощью внутреннего взора без труда увидел, кто подшутил над ним, и разгневался."
         text_warrior_01 = "Продолжить"
-        keyboard = types.ReplyKeyboardMarkup(
-            row_width=1, resize_keyboard=True)
+        user_states[message.chat.id] = 'continue_state'
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         bt_warrior_1 = types.KeyboardButton(text=text_warrior_01)
         keyboard.add(bt_warrior_1)
-
         bot.send_photo(message.chat.id, WarriorStory.old_man_002())
         bot.send_message(message.chat.id, text_warrior,
                          reply_markup=keyboard, parse_mode='html')
 
-    elif message.text == 'Продолжить':
-        text_warrior = "О, знатный воин! ты слишком гордишься своим положением и забыл, \n "
-        'что такое уважение. За это я проклинаю тебя! Ты потеряешь свое богатство и будешь скитаться, покинутый всеми!'
+    elif user_states.get(message.chat.id) == 'continue_state' and message.text == 'Продолжить':
+        # Обработка продолжения после "Уйти в свой замок"
+        text_warrior = "О, знатный воин! ты слишком гордишься своим положением и забыл, \n " \
+            "что такое уважение. За это я проклинаю тебя! Ты потеряешь свое богатство и будешь скитаться, покинутый всеми!"
+        # Завершение игры (удаление состояния)
+        user_states[message.chat.id] = 'continue_state_1'
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        bt_warrior_1 = types.KeyboardButton(text="Продолжить")
+        keyboard.add(bt_warrior_1)
+        bot.send_message(message.chat.id, text_warrior,
+                         reply_markup=keyboard, parse_mode='html')
 
+    elif user_states.get(message.chat.id) == 'continue_state_1' and message.text == 'Продолжить':
+        text_warrior = "В тот же миг твои замки вспыхнули, и только несколько слуг смогли спастись из огня. Твоя семья погибла, а на самого тебя напали лютые бандиты. Bзраненный, "
+        "ты бросаешься в реку и, ухватившись за бревнышко, плывешь вниз по течению. вскоре тебя спасают рыбаки, и ты узнаешь, что случилось с твоими замками и семьей."
+        text_warrior_4 = "Желая вернуть утраченное, ты решаешь отправиться в столицу, чтобы просить помощи у императора. Но куда приведет тебя это путешествие?"
         text_warrior_01 = "Продолжить"
+        user_states[message.chat.id] = 'continue_state_2'
         keyboard = types.ReplyKeyboardMarkup(
             row_width=1, resize_keyboard=True)
         bt_warrior_1 = types.KeyboardButton(text=text_warrior_01)
         keyboard.add(bt_warrior_1)
         bot.send_message(message.chat.id, text_warrior,
+                         text_warrior_4, reply_markup=keyboard, parse_mode='html')
+
+    elif user_states.get(message.chat.id) == 'continue_state_2' and message.text == "Желая вернуть утраченное, ты решаешь отправиться в столицу, чтобы просить помощи у императора. Но куда приведет тебя это путешествие?":
+        text_warrior_return = "На пути в столицу ты сталкиваешься с загадочным чародеем. Он предлагает помочь тебе в обмен на выполнение одной маленькой услуги."
+        text_warrior_return_01 = "Принять помощь"
+        user_states.pop(message.chat.id)
+        user_states[message.chat.id] = 'return_journey_state'
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        bt_warrior_1 = types.KeyboardButton(text=text_warrior_return_01)
+        keyboard.add(bt_warrior_1)
+        bot.send_message(message.chat.id, text_warrior_return,
                          reply_markup=keyboard, parse_mode='html')
 
-    elif message.text == 'Продолжить':
-        text_warrior = "В тот же миг твои замки вспыхнули, и только несколько слуг смогли спастись из огня. Твоя семья погибла, а на самого тебя напали лютые бандиты. Bзраненный, "
-        "ты бросаешься в реку и, ухватившись за бревнышко, плывешь вниз по течению. вскоре тебя спасают рыбаки, и ты узнаешь, что случилось с твоими замками и семьей."
-
-        text_warrior_01 = "Продолжить"
-        keyboard = types.ReplyKeyboardMarkup(
-            row_width=1, resize_keyboard=True)
-        bt_warrior_1 = types.KeyboardButton(text=text_warrior_01)
+    elif user_states.get(message.chat.id) == 'return_journey_state' and message.text == 'Принять помощь':
+        # Обработка события после "Желая вернуть утраченное"
+        text_warrior_return_accept = "Чародей помог тебе восстановить часть твоего богатства. Теперь ты в долгу перед ним."
+        text_warrior_return_01 = "Продолжить"
+        # Завершение игры (удаление состояния)
+        user_states.pop(message.chat.id)
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        bt_warrior_1 = types.KeyboardButton(text=text_warrior_return_01)
         keyboard.add(bt_warrior_1)
-        bot.send_message(message.chat.id, text_warrior, parse_mode='html')
+        bot.send_message(message.chat.id, text_warrior_return_accept,
+                         reply_markup=keyboard, parse_mode='html')
 
 ###################################################################################################################################################
 
@@ -159,10 +186,10 @@ class PrologWarrior:
                 message.chat.id, '<b>Игра началась!</b>', parse_mode='html')
 
 
-try:
-    # Запуск бота
-    if __name__ == '__main__':
+if __name__ == '__main__':
+    try:
+        # Запуск бота
         bot.polling(none_stop=True)
-        bot.infinity_polling()
-except:
-    print('Ошибка запуска бота!')
+        # bot.infinity_polling()  # Вы можете использовать одну из этих строк, но не обе
+    except Exception as e:
+        print(f'Ошибка запуска бота: {e}')
